e464965295f8ca2ac2b9e71b0b3f40b0d9e0e053
zulcas <zulemacasadogarcia@gmail.com>
2024-05-09 19:50:30 +0200

    ejercicio medio resuelto

--- app.js
+++ app.js
@@ -3,13 +3,34 @@
 // Variables de estado iniciales. Puede que falta alguna...
 
 let estoyJugando = true;
-let numeroCorrecto = Math.floor(Math.random() * 100);
+let numeroCorrecto = Math.floor(Math.random() * 10);
 let numIntentos = 10;
+let numUser 
+let previousGuesses = [];
+
+function refreshVariables{
+  document.querySelector('.guesses').textContent = previousGuesses;
+  document.querySelector('.lastResult').textContent = numIntentos;
+  document.querySelector
+
+}
 
 // Actualizar el innerHTML del nodo adecuado con el número de intentos iniciales
 
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault(); // Impedimos que el formulario haga un petición GET
+  console.log(e);
+  //Capturamos elemento usuario
+  numUser = document.querySelector('#guessField').value;
+  // Comprobar si el número que ha puesto el usuario es el numeroCorrecto
+  if (numUser == numeroCorrecto){
+  }
+  else{
+    if (numIntentos>0){
+      numIntentos--;
+      previousGuesses.push(numUser);
 
-  // Comprobar si el número que ha puesto el usuario es mayor o menor que el numeroCorrecto. Tomar decisiones
+    }
+
+  }
 });
